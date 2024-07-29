42e3905ea43b3ed6c643c6e643048fcf4efb9eb7
Vishrut Golakiya <101960778+vishrut-golakiya@users.noreply.github.com>
2024-07-29 14:49:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home - Demo Website</title>
+    <title>Home - PHP Website</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
