8aa1a75dc543e76a021d8899a632aed344079aee
Vishrut Golakiya <101960778+vishrut-golakiya@users.noreply.github.com>
2024-07-29 11:28:13 +0530

    Update install_nginx.sh

--- scripts/install_nginx.sh
+++ scripts/install_nginx.sh
@@ -3,28 +3,28 @@
 sudo apt-get update
 sudo apt-get install -y nginx
 
-# sudo apt install php-fpm php-mysql -y
+sudo apt install php-fpm php-mysql -y
 
 
-# sudo tee /etc/nginx/sites-available/default > /dev/null <<EOL
-# server {
-#     listen 80;
-#     server_name _;
+sudo tee /etc/nginx/sites-available/default > /dev/null <<EOL
+server {
+    listen 80;
+    server_name _;
 
-#     root /var/www/html/demo-ec2-pipeline;
-#     index index.php index.nginx-debian.html;
+    root /var/www/html;
+    index index.php index.nginx-debian.html;
 
-#     location / {
-#         try_files \$uri \$uri/ /index.php?\$args;
-#     }
+    location / {
+        try_files \$uri \$uri/ /index.php?\$args;
+    }
 
-#     location ~ \.php$ {
-#         include snippets/fastcgi-php.conf;
-#         fastcgi_pass unix:/var/run/php/php8.3-fpm.sock;
-#     }
+    location ~ \.php$ {
+        include snippets/fastcgi-php.conf;
+        fastcgi_pass unix:/var/run/php/php8.3-fpm.sock;
+    }
 
-#     location ~ /\.ht {
-#         deny all;
-#     }
-# }
-# EOL
+    location ~ /\.ht {
+        deny all;
+    }
+}
+EOL
