b9966fde3662e116fcca410a58592d7e81a315c7
Vishrut Golakiya <101960778+vishrut-golakiya@users.noreply.github.com>
2024-07-29 16:02:14 +0530

    Update install_nginx.sh

--- scripts/install_nginx.sh
+++ scripts/install_nginx.sh
@@ -2,7 +2,6 @@
 
 sudo apt-get update
 sudo apt-get install -y nginx
-
 sudo apt install php-fpm php-mysql -y
 
 
@@ -10,26 +9,22 @@ sudo tee /etc/nginx/sites-available/default > /dev/null <<EOL
 server {
     listen 80;
     server_name _;
-
     root /var/www/html;
     index index.php index.nginx-debian.html;
-
     location / {
         try_files \$uri \$uri/ /index.php?\$args;
     }
-
     location ~ \.php$ {
         include snippets/fastcgi-php.conf;
         fastcgi_pass unix:/var/run/php/php8.3-fpm.sock;
     }
-
     location ~ /\.ht {
         deny all;
     }
 }
 EOL
+
+
 sudo cd /var/www/html
 sudo rm -rf index.nginx-debian.html
-
-
 sudo systemctl restart nginx
